a7fffc1bfeb841b46983a8bf1cc5c2b1e77df553
JulianQA <jfqa21julian@hotmail.com>
2023-01-12 12:39:49 -0500

    basic CategoriesBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import { AsideBar } from "./components/AsideBar/AsideBar";
+import { CategoriesBar } from "./components/CategoriesBar/CategoriesBar";
 import { Header } from "./components/Header/Header";
 
 function App() {
@@ -12,6 +13,9 @@ function App() {
         toggleAsideBar={toggleAsideBar}
       />
       <AsideBar toggleAsideBar={toggleAsideBar} />
+      <div className="home">
+        <CategoriesBar />
+      </div>
     </>
   );
 }
